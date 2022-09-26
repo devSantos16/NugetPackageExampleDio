b0405d04a9348c9fdda4ff09f8cbef99340ec660
devsantos16 <devsantos1999@gmail.com>
2022-09-26 15:10:23 -0300

    Deserialize Object e Atributo de Propriedades

--- Models/Venda.cs
+++ Models/Venda.cs
@@ -2,20 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Models.Venda
 {
     public class Venda
     {
+
         public int Id { get; set; }
+        [JsonProperty("Nome_Produto")]
         public string Produto { get; set; }
         public double Preco { get; set; }
-
-        public Venda(int id, string produto, double preco)
-        {
-            this.Id = id;
-            this.Produto = produto;
-            this.Preco = preco;
-        }
+        public DateTime DataVenda { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,17 +5,26 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        string conteudo = File.ReadAllText("Arquivos.json");
+        List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudo);
+        
+        foreach(var item in listaVenda){
+            Console.WriteLine(item.Id);
+            Console.WriteLine(item.Produto);
+            Console.WriteLine(item.DataVenda.ToString("dd/MM/yyyy"));
+        }
 
-        List<Venda> listaV = new List<Venda>();
-        Venda v = new Venda(1, "Abacaxi", 10.95);
-        Venda v2 = new Venda(1, "Abacaxi", 10.95);
+        
 
 
-        listaV.Add(v);
-        listaV.Add(v2);
-        string serializado = JsonConvert.SerializeObject(listaV, Formatting.Indented);
-        Console.WriteLine(serializado);
-        File.WriteAllText("Arquivos.json", serializado);
+        // List<Venda> listaV = new List<Venda>();
+        // Venda v = new Venda(1, "Abacaxi", 10.95, DateTime.Now);
+
+
+        // listaV.Add(v);
+        // string serializado = JsonConvert.SerializeObject(listaV, Formatting.Indented);
+        // Console.WriteLine(serializado);
+        // File.WriteAllText("Arquivos.json", serializado);
 
     }
 }
